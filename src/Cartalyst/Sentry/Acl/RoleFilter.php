d8ba2095a0ee60610a4e914915305137dce587af
halfik <halfik@gmail.com>
2014-09-19 09:58:29 +0200

    filterek do rol

--- src/Cartalyst/Sentry/Acl/RoleFilter.php
+++ src/Cartalyst/Sentry/Acl/RoleFilter.php
@@ -3,4 +3,18 @@
 
 class RoleFilter {
 
+    /**
+     * filter dla query
+     * @param QueryBuilder $q
+     * @param User $user
+     * @param string $routeName
+     */
+    public function filter($q, $user, $routeName){
+        $mainGroup = $user->getMainGroup();
+        $config = \Config::get('acl');
+
+        if (isSet($config[$mainGroup->getCode()][$routeName])){
+            $config[$mainGroup->getCode()][$routeName]($q,$user);
+        }
+    }
 }
