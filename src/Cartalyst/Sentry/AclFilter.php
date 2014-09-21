50ee1179961a4bbab7c0467f6baeb0a6b598e3b3
halfik <halfik@gmail.com>
2014-09-21 12:30:34 +0200

    fix do acl filter

--- src/Cartalyst/Sentry/AclFilter.php
+++ src/Cartalyst/Sentry/AclFilter.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-use Cartalyst\Sentry\Acl\Filter as Filter;
+use Cartalyst\Sentry\Acl\RouteFilter as RouteFilter;
 
 
-class AclFilter extends  Filter {
+class AclFilter extends  RouteFilter {
 }
