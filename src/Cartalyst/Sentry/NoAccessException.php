d0ed3f2b9c57b87acb16bff04270a8d813af1311
Marcin Derlukiewicz <marcin.derlukiewicz@netinteractive.pl>
2014-09-25 10:59:29 +0200

    noAccess logowanie updawnien i requestów

--- src/Cartalyst/Sentry/NoAccessException.php
+++ src/Cartalyst/Sentry/NoAccessException.php
@@ -5,6 +5,22 @@ namespace Cartalyst\Sentry;
  * Class NoAccessException
  * ACL no access Exception
  */
-class NoAccessException extends \Exception {
 
+use \Illuminate\Support\Facades\Route as Route;
+
+class NoAccessException extends \Exception {
+    public function __construct($message="", $code=E_USER_ERROR, $previous=null) {
+        $route= Route::getCurrentRoute();
+        $message.="\ndenied with route ".$route->getUri();
+        $message.="\n".$route->getActionName()."[".$route->getName()."]";
+        $user = \App::make('sentry')->getUser();
+        //var_dump($user);
+        //\Log::info($user);
+        if ( !empty($user) && $user->isActivated()){
+            $message.="\nwith rights: ".print_r($user->getMergedPermissions(),1);
+	}else{
+            $message.=' without active user';
+        }
+        parent::__construct($message, $code, $previous);
+    }
 }
