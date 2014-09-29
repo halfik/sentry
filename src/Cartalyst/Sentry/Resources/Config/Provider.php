07a58d178b42845a378f7491bf8638e66a6c4066
Marcin Derlukiewicz <marcin.derlukiewicz@netinteractive.pl>
2014-09-29 11:13:27 +0200

    Revert "Revert "sentry fix for user acces at logout""
    This reverts commit 2fd5cddcf63eaed874b9c7aa7ca885869f8d3164.

---
+++ src/Cartalyst/Sentry/Resources/Config/Provider.php
@@ -0,0 +1,15 @@
+<?php namespace Cartalyst\Sentry\Resources\Config;
+
+use Cartalyst\Sentry\Resources\ProviderInterface;
+use Cartalyst\Sentry\Resources\ResourceNotFoundException;
+
+/**
+ * Description of Provider
+ *
+ * @author Marcin
+ */
+class Provider implements ProviderInterface {
+
+    public 
+
+}
